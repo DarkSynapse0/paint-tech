0e327143008486520cb5100b1b84aa6867a6921b
DarkSynapse0 <mannish079@gmail.com>
2025-06-19 16:19:28 +0545

    hh

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -29,10 +29,10 @@ const Contact = () => {
 
     try {
       await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        "service_a1pppoa",
+        "template_xoaj6yl",
         templateParams,
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        "3yti-fm9mHEDsIQDo"
       );
       setSuccessMessage("Your message has been sent successfully!");
       setFormData({
